47a7ab1760ea33631255c0469bb6234f7b2d8894
Nay Min Lwin <nayminlwin@gmail.com>
2026-05-11 11:51:21 +0630

    fix: remove credit_limit change check

--- erpnext/selling/doctype/customer/customer.py
+++ erpnext/selling/doctype/customer/customer.py
@@ -50,7 +50,7 @@ class Customer(TransactionBase):
 		self.flags.is_new_doc = self.is_new()
 		self.flags.old_lead = self.lead_name
 		validate_party_accounts(self)
-		self.validate_credit_limit_on_change()
+		# self.validate_credit_limit_on_change()
 		self.set_loyalty_program()
 		self.check_customer_group_change()
 
